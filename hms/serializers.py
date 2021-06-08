56c28158c6bccc91d44bccbcfdaad385215d6efe
Kasparov <ianjkiplagat@gmail.com>
2021-06-08 20:28:23 +0300

    add registration method

--- hms/serializers.py
+++ hms/serializers.py
@@ -1,5 +1,34 @@
 from rest_framework import serializers
 from .models import *
+from django import forms
+
+class RegistrationSerializer(serializers.ModelSerializer):
+  email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+  
+  password2 = serializers.CharField(style={'input_type': 'password'}, write_only=True)
+  class Meta:
+    model = User
+    fields = ['username','email','password', 'password2']
+    extra_kwargs = {
+      "password": {'write_only': True}
+    }
+    
+  def save(self):
+    user = User(
+            username = self.validated_data['username'],
+            email = self.validated_data['email'],
+        )
+    password = self.validated_data['password']
+    password2 = self.validated_data['password2']
+      
+      
+    if password != password2:
+      raise serializers.ValidationError({'password': 'Passwords must match'}) 
+    
+    user.set_password(password)
+    user.save()
+    return user
+
 
 class DoctorSerializer(serializers.ModelSerializer):
     class Meta:
--- hms/views.py
+++ hms/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
 from .models import *
 from datetime import datetime,timedelta,date
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from django.http import Http404
+from django.http import response
+from .serializers import *
 
-# Create your views here.
+class Registration(APIView):
+  serializer_class=RegistrationSerializer
+
+  def post(self, request):
+      serializer=self.serializer_class(data=request.data)
+      serializer.is_valid(raise_exception=True)
+      serializer.save()
+
+      user=serializer.data
+
+      response={
+          "data":{
+              "user":dict(user),
+              "status":"Success",
+              "message":"User account created successfully"
+          }
+
+      }
+      return Response(response, status=status.HTTP_201_CREATED)
