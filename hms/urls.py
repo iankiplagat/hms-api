788241cc31e9412c6d491002bb2886ed89f50ba3
Kasparov <ianjkiplagat@gmail.com>
2021-06-08 21:12:30 +0300

    add login method

--- hms/urls.py
+++ hms/urls.py
@@ -2,11 +2,14 @@ from django.urls import path, re_path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-#..............
+from rest_framework.authtoken.views import ObtainAuthToken
+
 
 urlpatterns=[
     # Authentication
   path('register/', views.Registration.as_view(), name="register"),
+  path('login/', views.LoginUser.as_view(), name="login"),
+  path('authlogin/', ObtainAuthToken.as_view(), name="authlogin"),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- hms/views.py
+++ hms/views.py
@@ -4,6 +4,9 @@ from datetime import datetime,timedelta,date
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authtoken.models import Token
 from django.http import Http404
 from django.http import response
 from .serializers import *
@@ -27,3 +30,26 @@ class Registration(APIView):
 
       }
       return Response(response, status=status.HTTP_201_CREATED)
+    
+    
+class LoginUser(APIView):
+    serializer_class=LoginSerializer
+    authentication_classes=(TokenAuthentication,)
+    permission_classes=(IsAuthenticated,)
+
+        # login user
+    def post(self, request, format=None):
+        serializers=self.serializer_class(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            users=serializers.data
+
+            response={
+                "data":{
+                    "user":dict(users),
+                    "status":"Success",
+                    "message":"User logged in successfully"
+                }
+            }
+            return Response(response, status=status.HTTP_200_OK)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)    
