9126ab49e363788c5ebff55f771b53ce0173ff0b
Kasparov <ianjkiplagat@gmail.com>
2021-06-08 19:34:49 +0300

    configure postgres

--- hmsystem/settings.py
+++ hmsystem/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'hmsystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hms',
+        'USER': 'kasparov',
+        'PASSWORD':'ian@2304',
     }
 }
 
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
